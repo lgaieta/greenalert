69c7f335f277d02cc4a41a5f4d74815b4d3acb78
lgaieta.dev <lgaieta.dev@gmail.com>
2024-06-09 03:23:16 +0000

    Added horizontal padding in register page

--- app/(auth)/crear-cuenta/page.tsx
+++ app/(auth)/crear-cuenta/page.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from '@/components/register-form'
 
 function RegisterPage() {
     return (
-        <main className='mx-auto max-w-md space-y-6 py-12'>
+        <main className='mx-auto max-w-md space-y-6 py-12 px-4'>
             <header className='flex flex-col gap-2 text-center'>
                 <h1 className='text-3xl font-bold'>Crear cuenta</h1>
                 <p className='text-gray-500 dark:text-gray-400'>
