ee2d4b93663bb3de271225197e4f6efe87e01571
lgaieta.dev <lgaieta.dev@gmail.com>
2024-06-09 03:08:44 +0000

    Added static register page

---
+++ app/(auth)/crear-cuenta/page.tsx
@@ -0,0 +1,17 @@
+import { RegisterForm } from '@/components/register-form'
+
+function RegisterPage() {
+    return (
+        <main className='mx-auto max-w-md space-y-6 py-12'>
+            <header className='flex flex-col gap-2 text-center'>
+                <h1 className='text-3xl font-bold'>Crear cuenta</h1>
+                <p className='text-gray-500 dark:text-gray-400'>
+                    El primer paso para empezar a visualizar
+                </p>
+            </header>
+            <RegisterForm />
+        </main>
+    )
+}
+
+export default RegisterPage
