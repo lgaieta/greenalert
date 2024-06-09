bdbfe1aaac5253d50038d2f9800d53231adbaf5e
lgaieta.dev <lgaieta.dev@gmail.com>
2024-06-09 03:36:05 +0000

    Fixed responsive in RegisterPage

--- app/(auth)/crear-cuenta/page.tsx
+++ app/(auth)/crear-cuenta/page.tsx
@@ -2,14 +2,16 @@ import { RegisterForm } from '@/components/register-form'
 
 function RegisterPage() {
     return (
-        <main className='mx-auto max-w-md space-y-6 py-12 px-4'>
-            <header className='flex flex-col gap-2 text-center'>
-                <h1 className='text-3xl font-bold'>Crear cuenta</h1>
-                <p className='text-gray-500 dark:text-gray-400'>
-                    El primer paso para empezar a visualizar
-                </p>
-            </header>
-            <RegisterForm />
+        <main className='flex gap-4 justify-center items-center min-h-screen w-full py-12'>
+            <div className='max-w-sm w-full space-y-6'>
+                <header className='flex flex-col gap-2 text-center'>
+                    <h1 className='text-3xl font-bold'>Crear cuenta</h1>
+                    <p className='text-gray-500 dark:text-gray-400'>
+                        El primer paso para empezar a visualizar
+                    </p>
+                </header>
+                <RegisterForm />
+            </div>
         </main>
     )
 }
