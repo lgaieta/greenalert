9e6bec0fd66e8e2d82338f085ecd359a61726b46
lgaieta.dev <lgaieta.dev@gmail.com>
2024-06-09 16:52:55 +0000

    Added hero section in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Hero } from '@/components/hero'
+
 export default function Home() {
     return (
-        <main className='flex min-h-screen flex-col items-center text-center gap-4 justify-center p-24'>
-            <h1 className='text-4xl font-bold'>GreenAlert</h1>
-            <p>Mapeo de problemas ambientales</p>
+        <main className='flex min-h-screen flex-col items-center gap-4 justify-center'>
+            <Hero />
         </main>
     )
 }
