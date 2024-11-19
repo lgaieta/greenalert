e1a535c21cad2042f48d23b31c6358bc9e6344d4
lgaieta <lgaieta.dev@gmail.com>
2024-11-18 22:24:05 -0300

    Added content when student has no course in new report page

--- app/nuevo-problema/page.tsx
+++ app/nuevo-problema/page.tsx
@@ -1,11 +1,36 @@
 import NewReportForm from '@/components/new-report/new-report-form'
+import { Button } from '@/components/ui/button'
+import UserType from '@/lib/entities/UserType'
+import CourseRepository from '@/lib/services/CourseRepository'
 import SessionManager from '@/lib/services/SessionManager'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 async function NewReportPage() {
-    const { authorized } = await SessionManager.validateSession()
+    const { authorized, usertype, email } = await SessionManager.validateSession()
 
-    if (!authorized) return redirect('/')
+    if (!authorized || usertype !== UserType.Student) return redirect('/')
+
+    const course = await CourseRepository.getByStudentEmail(email)
+
+    if (course === null)
+        return (
+            <main className='flex gap-4 justify-center items-center h-full w-full py-12 lg:py-24'>
+                <div className='flex flex-col items-center w-full space-y-10'>
+                    <header className='max-w-sm flex flex-col items-center gap-4 text-center'>
+                        <h1 className='text-xl font-bold'>
+                            Debes unirte a un curso antes de reportar un problema
+                        </h1>
+                        <Button
+                            asChild
+                            className='w-fit'
+                        >
+                            <Link href={'/cursos/unirme'}>Quiero unirme a un curso</Link>
+                        </Button>
+                    </header>
+                </div>
+            </main>
+        )
 
     return (
         <main className='flex gap-4 justify-center items-center h-full w-full py-12 lg:py-24'>
